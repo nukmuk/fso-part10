db96632b3fba7e15d708012958d58833437a88db
poju <rasmus.hintzmadsen@gmail.com>
2024-07-10 16:31:57 +0300

    10.4 done

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,31 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import Constants from "expo-constants";
+import Text from "./Text";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.backgroundColor,
+    // ...
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable style={{ padding: 20 }}>
+        <Text
+          fontSize={"subheading"}
+          fontWeight="bold"
+          style={{ color: "white" }}
+        >
+          Repositories
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
