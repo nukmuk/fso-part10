440b4b413c12c200f530380c47c4036fc99cb80f
poju <rasmus.hintzmadsen@gmail.com>
2024-07-17 18:38:48 +0300

    10.20 done

--- src/components/SingleRepositoryInfo.jsx
+++ src/components/SingleRepositoryInfo.jsx
@@ -3,19 +3,85 @@ import RepositoryItem from "./RepositoryItem";
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORY } from "../graphql/queries";
 import Text from "./Text";
+import { FlatList, StyleSheet, View } from "react-native";
+import theme from "../theme";
+import { format } from "date-fns";
+
+const styles = StyleSheet.create({
+  review: {
+    container: {
+      marginTop: 16,
+      backgroundColor: "white",
+      padding: 16,
+      flexDirection: "row",
+      gap: 16,
+    },
+    rating: {
+      width: 48,
+      height: 48,
+      borderRadius: 24,
+      borderColor: theme.colors.primary,
+      borderWidth: 2,
+      borderStyle: "solid",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "red",
+    },
+    ratingText: {
+      color: theme.colors.primary,
+    },
+    rightContainer: {
+      gap: 4,
+    },
+  },
+});
+
+const SingleRepositoryInfoHeader = ({ repository }) => {
+  return <RepositoryItem repository={repository} expanded={true} />;
+};
+
+const ReviewItem = ({ review }) => {
+  return (
+    <View style={styles.review.container}>
+      <View style={styles.review.rating}>
+        <Text fontSize="subheading" fontWeight="bold" color="primary">
+          {review.rating}
+        </Text>
+      </View>
+      <View style={styles.review.rightContainer}>
+        <Text fontWeight="bold">{review.user.username}</Text>
+        <Text color="textSecondary">
+          {format(review.createdAt, "dd.MM.yyyy")}
+        </Text>
+        <Text>{review.text}</Text>
+      </View>
+    </View>
+  );
+};
 
 const SingleRepositoryInfo = () => {
   const { id } = useParams();
-  console.log("single id", id);
   const { data, loading, error } = useQuery(GET_REPOSITORY, {
     variables: { id },
   });
-  console.log("data", data);
 
   if (error) return <Text>Error: {error.message}</Text>;
   if (loading) return <Text>Loading {id}...</Text>;
 
-  return <RepositoryItem repository={data.repository} expanded={true} />;
+  const repository = data.repository;
+
+  const reviews = repository.reviews.edges.map((edge) => edge.node);
+
+  return (
+    <FlatList
+      data={reviews}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => (
+        <SingleRepositoryInfoHeader repository={repository} />
+      )}
+    ></FlatList>
+  );
 };
 
 export default SingleRepositoryInfo;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -32,6 +32,20 @@ export const GET_REPOSITORY = gql`
     repository(id: $id) {
       ...RepositoryData
       url
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
   ${REPOSITORY_DATA}
